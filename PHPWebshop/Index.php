5104dfd412ec2e996cc7dbe16c240a6971568fc8
Spirit-Balanceholder <martinkleinpaste@hotmailcom>
2014-03-14 11:27:39 +0100

    A Proper Welcome

--- PHPWebshop/Index.php
+++ PHPWebshop/Index.php
@@ -11,8 +11,56 @@
 	?>
 	<div class = "wrap container">
 		<div class="mainbody">
-		
-		
+			<div class="Welcome">
+				<h1>Welcome to Green Dragons Lair</h1>
+				<h2>The place for all your fantasy needs</h2>
+			</div>
+			
+			<div class="row">
+				
+				<div class="col-md-4">
+					<div class="panel panel-success">
+						<div class="panel-heading">
+							<h3>Weekly Offer</h3>
+						</div>
+						<div class="panel-body">
+							<a class="thumbnail" href = "Webpages/Product.php">
+							<img src="Webpages/img/DummyProduct.jpg">
+							Computah
+							</a>
+						</div>
+					</div>				
+				</div>
+				
+				<div class="col-md-4">
+					<div class="panel panel-success">
+						<div class="panel-heading">
+							<h3>Most Popular</h3>
+						</div>
+						<div class="panel-body">
+							<a class="thumbnail" href = "Webpages/Product.php">
+							<img src="Webpages/img/DummyProduct.jpg">
+							Computah
+							</a>
+						</div>
+					</div>				
+				</div>
+				
+				<div class="col-md-4">
+					<div class="panel panel-success">
+						<div class="panel-heading">
+							<h3>Newest Addition</h3>
+						</div>
+						<div class="panel-body">
+							<a class="thumbnail" href = "Webpages/Product.php">
+							<img src="Webpages/img/DummyProduct.jpg">
+							Computah
+							</a>
+						</div>
+					</div>				
+				</div>
+				
+			</div>
 		</div>
 	</div>
 	<?php 
