ad433554dc79116a7d82d71cf3cd4b8a63c2f2a1
mjowned <mj65@live.nl>
2014-03-18 17:09:23 +0100

    Product.php code getest

--- PHPWebshop/Webpages/Product.php
+++ PHPWebshop/Webpages/Product.php
@@ -16,18 +16,32 @@
 				
 				include_once $_SERVER ['DOCUMENT_ROOT'] . '/Classes/InfoControllers/Product.php';
 				$productInfo = new Product();
-					$result = $productInfo->getProductById($prodID);
+				$result = $productInfo->getProductById($prodID);
 				
-				while ($line = mysqli_fetch_array($result, MYSQL_ASSOC)) {
-				echo "<tr>";
-				foreach ($line as $col_value) {
-					echo "<td>$col_value</td>";
-					if ($col_value == end($line)){
+				$display = $result->fetch_assoc(); //gebruik display om data van het product te displayen
+				echo "Name: <br><b>".$display['Name']."</b><br><br>"; //example
+				
+				?>	
+					
+				<?php 
+				echo "Description: <br>".$display['LongDescription']."<br><br>";
+				echo '<img src=http://'.$_SERVER['SERVER_NAME'].':'
+					.$_SERVER['SERVER_PORT'].dirname($_SERVER['REQUEST_URI'])
+					.$display['Image_path'].'></img><br><br>'
+				
+				/*
+				 * simpel test while loopje
+				 */
+// 				while ($line = mysqli_fetch_array($result, MYSQL_ASSOC)) {
+// 				echo "<tr>";
+// 				foreach ($line as $col_value) {
+// 					echo "<td>$col_value</td>";
+// 					if ($col_value == end($line)){
 						
-					}
-				}
-				echo "\t</tr>\n";
-				}
+// 					}
+// 				}
+// 				echo "\t</tr>\n";
+// 				}
 				?>
 				
 			</div>
