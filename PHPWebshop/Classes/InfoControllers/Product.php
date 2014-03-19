93efaaa8f66229b02dc7882a710df1dc085afdac
mjowned <mj65@live.nl>
2014-03-19 15:26:18 +0100

    create product admin bezig

--- PHPWebshop/Classes/InfoControllers/Product.php
+++ PHPWebshop/Classes/InfoControllers/Product.php
@@ -30,8 +30,20 @@ class Product{
 	/*
 	 * //TODO not done
 	 */
-	public function insertNewProduct(){
-		
+	public function insertNewProduct(){ //nog werkend maken, eerst in admin/product werken
+		include_once $_SERVER ['DOCUMENT_ROOT'] . '/Classes/DatabaseConnectie/Database.php';
+		if (! empty ($values)){
+			$db = new Database ();
+			$db->insert("Product",array(
+					$values ["prodname"],
+					$values ["price"],
+					$values ["sdescription"],
+					$values ["designed_by"],
+					$values ["estimated_days"],
+					$values ["ldescription"])
+					, array ("Firstname", "Middlename", "Surname",
+							"Studentnumber", "Email", "Location_ID"));
+		}
 	}
 }
 ?>
